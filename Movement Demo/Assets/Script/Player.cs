350c868a6e0190b531f242d96ed9164c1e69a769
Devyn Theis <dtheis2@illinois.edu>
2015-10-02 12:30:21 -0500

    Movement Demo

---
+++ Movement Demo/Assets/Script/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	float maxSpeed = 3.0f;
+	float speed = 50.0f;
+	//float jumpPower = 150.0f;
+	bool grounded = true;
+
+	Rigidbody2D rb2d;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = gameObject.GetComponent<Rigidbody2D>();
+		anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool ("Grounded", grounded);
+		anim.SetFloat ("Speed", Mathf.Abs (Input.GetAxis ("Horizontal")));
+        fixedUpdate();
+	}
+
+	void fixedUpdate(){
+		// Player Movement
+		float h = Input.GetAxis ("Horizontal");
+		rb2d.AddForce((Vector2.right * speed) * h);
+		
+		//  Limits the speed of the player.
+		if (rb2d.velocity.x > maxSpeed) {
+			rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+		}
+		if (rb2d.velocity.x < -maxSpeed) {
+			rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+		}
+	}
+}

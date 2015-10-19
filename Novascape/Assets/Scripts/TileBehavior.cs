641f5c9fab37e3ed8fe0a42e7cd65825f11efdde
Austin Bae <1551780@gmail.com>
2015-10-19 18:20:23 -0500

    Added Tile Class and MouseClicker

---
+++ Novascape/Assets/Scripts/TileBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileBehavior : MonoBehaviour {
+
+	private int durability = 3;
+	public Texture2D tileText;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag ("Tool")) {
+			durability--;
+		}
+		if (durability == 0) {
+			InventoryGUI script = GameObject.Find ("Inventory").GetComponent<InventoryGUI>();
+			script.add (1, "Black Tile", tileText, "Simple");
+			this.gameObject.SetActive (false);
+		}
+	}
+}
---
+++ Novascape/Assets/Scripts/mouseControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseControl : MonoBehaviour {
+
+	private Vector2 mousePosition;
+	public float moveSpeed = 0.1f;
+	private int durability = 20;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mousePosition = Input.mousePosition;
+		mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+		transform.position = Vector2.Lerp(transform.position, mousePosition,moveSpeed);
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag ("Tile")) {
+			durability --;
+		}
+		if (durability == 0) {
+			this.gameObject.SetActive (false);
+		}
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (20, 20, 100, 100), durability.ToString ());
+	}
+}

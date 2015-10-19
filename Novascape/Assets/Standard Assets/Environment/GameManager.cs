8ac43913d2778088efe32bde8e9647956d4b9518
Chris Jamrozy <cdjamrozy@urhwirelessprvnat-172-18-33-17.near.illinois.edu>
2015-10-19 18:09:34 -0500

    World Gen

---
+++ Novascape/Assets/Standard Assets/Environment/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance= null;
+	private Planet planet1;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+
+		planet1 = GetComponent<Planet>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
